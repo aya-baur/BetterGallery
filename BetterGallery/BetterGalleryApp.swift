177b93140acc59eb1bde1c67380f68846a3f8b2d
Aya Bauyrzhankyzy <aya.baur@gmail.com>
2022-05-09 14:51:20 -0400

    Initial Commit

---
+++ BetterGallery/BetterGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterGalleryApp.swift
+//  BetterGallery
+//
+//  Created by Aya Baur on 2022-05-09.
+//
+
+import SwiftUI
+
+@main
+struct BetterGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterGallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterGallery
+//
+//  Created by Aya Baur on 2022-05-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
